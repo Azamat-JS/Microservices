ab3c7bec53da74177261c124d0ca04c85c52f1cf
Azamat-JS <azamatielts987@gmail.com>
2025-05-20 10:14:00 +0500

    CRUD operations completed and tested with gRPC

--- nestjs-grpc/apps/api-gateway/src/app/product/product.controller.ts
+++ nestjs-grpc/apps/api-gateway/src/app/product/product.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Inject, OnModuleInit, Param } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Inject, OnModuleInit, Param, Post, Put } from "@nestjs/common";
 import { ClientGrpc } from "@nestjs/microservices";
-import { PRODUCTS_PACKAGE_NAME, ProductServiceClient, PRODUCT_SERVICE_NAME } from "types/proto/products";
+import { PRODUCTS_PACKAGE_NAME, ProductServiceClient, PRODUCT_SERVICE_NAME, CreateProductDto, UpdataProductDto } from "types/proto/products";
 
 @Controller('product')
 export class ProductController implements OnModuleInit{
@@ -13,9 +13,30 @@ export class ProductController implements OnModuleInit{
         this.productService = this.client.getService<ProductServiceClient>(PRODUCT_SERVICE_NAME);
     }
 
+    @Post()
+    createProduct(@Body() createProductDto: CreateProductDto){
+        return this.productService.craeteProduct(createProductDto)
+    }
+
+    @Get('all')
+    findAll(){
+        return this.productService.findAllProducts({})
+    }
+
     @Get(':id')
     findOne(@Param('id') id:string){
         const numId = parseInt(id)
         return this.productService.getProduct({productId: numId})
+    }
+
+    @Put('update')
+    updateProduct(@Body() updateProductDto: UpdataProductDto){
+        console.log(updateProductDto)
+        return this.productService.updateProduct(updateProductDto)
+    }
+
+    @Delete('delete/:id')
+    deleteProduct(@Param('id') id: number){
+        return this.productService.deleteProduct({productId: id})
     }
 }
--- nestjs-grpc/apps/products/src/app/product.controller.ts
+++ nestjs-grpc/apps/products/src/app/product.controller.ts
@@ -13,56 +13,58 @@ import {
 @Controller()
 @ProductServiceControllerMethods()
 export class ProductController implements ProductServiceController {
+  async craeteProduct(request: CreateProductDto): Promise<ProductResponse> {
+    console.log(request);
+    return {
+      productId: 100,
+      ...request,
+    };
+  }
 
- async craeteProduct(request: CreateProductDto): Promise<ProductResponse> {
+  async getProduct(request: ProductRequest): Promise<ProductResponse> {
     return {
       productId: 1,
       name: "Phone",
-      price: 13
-    }
+      price: 13,
+    };
   }
 
- async getProduct(request: ProductRequest): Promise<ProductResponse> {
+  async findAllProducts(request: Empty): Promise<Products> {
     return {
-      productId: 1,
-      name: "Phone",
-      price: 13
-    }
+      users: [
+        {
+          productId: 1,
+          name: "Phone",
+          price: 13,
+        },
+        {
+          productId: 1,
+          name: "Phone",
+          price: 13,
+        },
+        {
+          productId: 1,
+          name: "Phone",
+          price: 13,
+        },
+      ],
+    };
   }
 
- async findAllProducts(request: Empty): Promise<Products> {
-    return {users: [
-      {
-      productId: 1,
-      name: "Phone",
-      price: 13
-    },
-    {
-      productId: 1,
-      name: "Phone",
-      price: 13
-    },
-    {
-      productId: 1,
-      name: "Phone",
-      price: 13
-    }
-    ] }
-  }
-
- async updateProduct(request: UpdataProductDto): Promise<ProductResponse> {
+  async updateProduct(request: UpdataProductDto): Promise<ProductResponse> {
+    console.log(request);
     return {
-  productId: 110,
+      ...request,
       name: "Cat",
-      price: 85
-    }
+    };
   }
 
- async deleteProduct(request: ProductRequest): Promise<ProductResponse> {
+  async deleteProduct(request: ProductRequest): Promise<ProductResponse> {
+    console.log(request)
     return {
-      productId: 11,
-      name: "Hat",
-      price: 900
-    }
+      productId: request.productId,
+      name: "Phone",
+      price: 13,
+    };
   }
 }
