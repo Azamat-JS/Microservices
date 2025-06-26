da7826f6066312e2d8bb4e62a7120d5d6a1eaf39
Azamat-JS <azamatielts987@gmail.com>
2025-06-26 12:27:00 +0500

    implemented cotroller file for gRPC project

--- mono-gRPC/apps/auth/src/main.ts
+++ mono-gRPC/apps/auth/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { AUTH } from '@app/common';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AuthModule, {
+    transport: Transport.GRPC,
+    options: {
+      protoPath: join(__dirname, '../auth.proto'),
+      package: AUTH
+    }
+  });
+  await app.listen();
+  Logger.log('Auth service is listening to gRPC...')
 }
 bootstrap();
